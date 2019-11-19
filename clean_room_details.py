de6d361879db66890bcf58a9238ce48227831c77
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-19 13:49:29 +0100

    Second pass solution

--- clean_room_details.py
+++ clean_room_details.py
@@ -1,17 +1,28 @@
 import json
 
+# to hold the rooms
 room_details = []
 
 with open("room_details.py", "r") as f:
     # read the room details
     room_details = json.loads(f.read())
 
+print("Length before cleaning is: ", len(room_details))
+
 room_set = set()
+cleaned_rooms = []
 
 # add the rooms to set to remove repetition
 for room in room_details:
-    room_set.add(room)
+    # check if the room id is not in the set
+    if room["room_id"] not in room_set:
+        # add the room_id
+        room_set.add(room["room_id"])
+        # add it to the cleaned_room list
+        cleaned_rooms.append(room)
+
+print("Length after cleaning is: ", len(cleaned_rooms))
 
 # write the result to the cleaned room_details file
 with open("traverse_results/room_details.py", "w") as f:
-    f.write(json.dumps(room_set))
+    f.write(json.dumps(cleaned_rooms))

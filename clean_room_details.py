619a2e0b55d6726dfb9c96838fe8a10b0495ba71
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-19 14:17:28 +0100

    Improved commenting

--- clean_room_details.py
+++ clean_room_details.py
@@ -4,19 +4,21 @@ import json
 room_details = []
 
 with open("room_details.py", "r") as f:
-    # read the room details
+    # read room_details.py into room_details list
     room_details = json.loads(f.read())
 
 print("Length before cleaning is: ", len(room_details))
 
+# will hold room ids
 room_set = set()
+# will contain a single copy of each room
 cleaned_rooms = []
 
-# add the rooms to set to remove repetition
+# loop through every room in room_details
 for room in room_details:
     # check if the room id is not in the set
     if room["room_id"] not in room_set:
-        # add the room_id
+        # add the room id to set to remove repetition
         room_set.add(room["room_id"])
         # add it to the cleaned_room list
         cleaned_rooms.append(room)
